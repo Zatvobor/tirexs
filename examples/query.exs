1ed686b5d0730716ba8529a1f20d7429a0417e7c
Alex Opak <opak.alexandr@gmail.com>
2013-03-01 16:50:56 +0200

    add query example #59

---
+++ examples/query.exs
@@ -0,0 +1,46 @@
+use Tirexs.Search
+
+Tirexs.DSL.search [name: "tets_index"], fn(search, _elastic_settings) ->
+  search = search do
+    filter do
+      nested [path: "dna"] do
+        query do
+          filtered do
+            query do
+              match_all
+            end
+            filter do
+              nested [path: "dna.matrix"] do
+                query do
+                  filtered do
+                    query do
+                      match_all
+                    end
+                    filter do
+                      _or do
+                        filters do
+                          nested [path: "dna.matrix.sport"] do
+                            query do
+                              range "dna.matrix.sport.choice", from: 6, to: 6
+                            end
+                          end
+                          nested [path: "dna.matrix.sex"] do
+                            query do
+                              range "dna.matrix.sex.choice", from: 6, to: 6
+                            end
+                          end
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  [search, _elastic_settings]
+end
