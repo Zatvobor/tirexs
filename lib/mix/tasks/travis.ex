40f1393198df4084fc304f3a4910f79c19e19ad6
Zatvor <a.zatvornitskiy@gmail.com>
2013-03-01 12:26:40 +0200

    Moved tasks to lib/mix/tasks accordingly

--- lib/mix/tasks/travis.ex
+++ lib/mix/tasks/travis.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.Travis do
   use Mix.Task
 
+  @shortdoc "Runs only unit tests whereas acceptances are skipped"
+
   def run(_) do
     Mix.Task.run("test", ["test/tirexs"])
   end
