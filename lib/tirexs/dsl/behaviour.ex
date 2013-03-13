81869c611e23f11e8040806b318e644883dfc395
Zatvor <a.zatvornitskiy@gmail.com>
2013-03-13 17:50:58 +0200

    Added DSL.Behaviour, issue #88

---
+++ lib/tirexs/dsl/behaviour.ex
@@ -0,0 +1,13 @@
+defmodule Tirexs.DSL.Behaviour do
+  @moduledoc """
+  Defines a main contract for domain specific handlers.
+
+  """
+
+  use Behaviour
+
+  @doc """
+  Receives a quoted dsl block and return a flat data structure which should be used to JSON encoding.
+  """
+  defcallback transpose(block :: list) :: list
+end
