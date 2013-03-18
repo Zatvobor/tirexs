f47ab775e16e8f6c3656978e0a7282e5139acb14
Zatvor <a.zatvornitskiy@gmail.com>
2013-03-18 15:04:10 +0200

    Updated texts, issue #93

--- examples/search.exs
+++ examples/search.exs
@@ -2,18 +2,16 @@
 # Run this example from console manually:
 #
 #   mix run -r examples/search.exs
+#   # => curl -X POST -d '{"query":{"nested":{"query":{"bool":{"must":[{"match":{"comments.author":{"query":"John"}}},{"match":{"comments.message":{"query":"cool"}}}]}},"path":"comments"}}}' http://127.0.0.1:9200/tets_index/_search
+#
 # Run this example from Elixir environment:
-# => curl -X POST -d '{"query":{"nested":{"query":{"bool":{"must":[{"match":{"comments.author":{"query":"John"}}},{"match":{"comments.message":{"query":"cool"}}}]}},"path":"comments"}}}' http://127.0.0.1:9200/tets_index/_search
 #
 #   Tirexs.Loader.load Path.expand("examples/search.exs")
 #
-
-
 import Tirexs.Search
 
-Tirexs.DSL.define [index: "tets_index"], fn(_search, _elastic_settings) ->
-  # We'll use the `nested` query to search for articles where _John_ left a _"Cool"_ message:
-  #
+Tirexs.DSL.define [index: "tets_index"], fn(_search, elastic_settings) ->
+  # Let's consider the `nested` query for searching articles where user `John` has left a `Cool` message:
   search = search do
     query do
       nested [path: "comments"] do
@@ -31,9 +29,9 @@ Tirexs.DSL.define [index: "tets_index"], fn(_search, _elastic_settings) ->
 
   # This couple of code lines here just for `mix run -r examples/search.exs` command output
   # It's obvious that you should not use it in real search :)
-  url  = Tirexs.ElasticSearch.make_url(_search[:index] <> "/_search", _elastic_settings)
+  url  = Tirexs.ElasticSearch.make_url(_search[:index] <> "/_search", elastic_settings)
   json = Tirexs.Query.to_resource_json(search)
   IO.puts "# => curl -X POST -d '#{json}' #{url}"
 
-  { search ++ _search, _elastic_settings }
+  { search ++ _search, elastic_settings }
 end
