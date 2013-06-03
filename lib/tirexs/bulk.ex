038e1257ae0975c4dded605d87494ac9c540d0bc
Alex Opak <opak.alexandr@gmail.com>
2013-06-03 11:12:34 +0300

    close #104 works with 0.90.1 elasticsearch and up versions

--- lib/tirexs/bulk.ex
+++ lib/tirexs/bulk.ex
@@ -5,7 +5,6 @@ defmodule Tirexs.Bulk do
 
   defmacro store(options, settings, [do: block]) do
     documents = extract_block(block)
-    options = options
     quote do
       [documents, options, settings] = [unquote(documents), unquote(options), unquote(settings)]
       bulk(documents, options, settings)
@@ -24,15 +23,32 @@ defmodule Tirexs.Bulk do
     [index: opts]
   end
 
+	def update(opts) do
+		IO.puts "d"
+		[update: opts]
+	end
+
   def bulk(documents, options, settings) do
     index = options[:index]
+		retry_on_conflict = options[:retry_on_conflict]
+		options = Dict.delete(options, :retry_on_conflict)
     options = Dict.delete(options, :index)
+		id = options[:id]
+		options = Dict.delete(options, :id)
     payload = Enum.map documents, fn(document) ->
-      action = key(document)
+      document = match(document)
+			action = key(document)
       document = document[action]
       type = get_type_from_document(document)
-      id   = get_id_from_document(document)
-      header = [_index: index, _type: type, _id: id ]
+      unless id do
+				id   = get_id_from_document(document)
+			end
+
+			header = [_index: index, _type: type, _id: id ]
+			if retry_on_conflict do
+				header = Dict.put(header, :retry_on_conflict, retry_on_conflict)
+			end
+
       [document, meta] = meta([:_version, :_routing, :_percolate, :_parent, :_timestamp, :_ttl], document, header)
       header = Dict.put([], action, meta)
 
@@ -71,4 +87,15 @@ defmodule Tirexs.Bulk do
   def get_type_from_document(document) do
     document[:_type] || document["_type"] || document[:type] || document["type"] || "document"
   end
+
+	def match(document) do
+
+		case is_list(document) do
+			true -> document
+			false ->
+				{key, properties} = document
+				Dict.put([], key, properties)
+		end
+	end
+
 end
--- test/acceptances/bulk_test.exs
+++ test/acceptances/bulk_test.exs
@@ -5,27 +5,52 @@ defmodule Acceptances.BulkTest do
   import Tirexs.Bulk
 
   test :create do
-    settings = Tirexs.ElasticSearch.Config.new()
+    # settings = Tirexs.ElasticSearch.Config.new()
+    #     Tirexs.ElasticSearch.delete("bear_test", settings)
+    #
+    #     Tirexs.Bulk.store [index: "bear_test", refresh: false], settings do
+    #       create id: 1, title: "bar1", description: "foo bar test"
+    #       create id: 2, title: "bar2", description: "foo bar test"
+    #       create id: 3, title: "bar3", description: "foo bar test"
+    #       create id: 4, title: "bar4", description: "foo bar test"
+    #       create id: 5, title: "bar5", description: "foo bar test"
+    #       create id: 6, title: "bar6", description: "foo bar test"
+    #       create id: 7, title: "bar7", description: "foo bar test"
+    #       create id: 8, title: "bar8", description: "foo bar test"
+    #       create id: 9, title: "bar9", description: "foo bar test"
+    #       create id: 10, title: "bar10", description: "foo bar test"
+    #       create id: 11, title: "bar11", description: "foo bar test"
+    #       delete id: 11
+    #       index  id: 90, title: "barww"
+    #     end
+    #
+    #     Tirexs.Manage.refresh("bear_test", settings)
+    #     {_, _, body} = Tirexs.ElasticSearch.get("bear_test/_count", settings)
+    #     assert body["count"] == 11
+  end
+
+	test :update do
+		settings = Tirexs.ElasticSearch.Config.new()
     Tirexs.ElasticSearch.delete("bear_test", settings)
 
-    Tirexs.Bulk.store [index: "bear_test", refresh: false], settings do
+		Tirexs.Bulk.store [index: "bear_test", refresh: false], settings do
       create id: 1, title: "bar1", description: "foo bar test"
       create id: 2, title: "bar2", description: "foo bar test"
-      create id: 3, title: "bar3", description: "foo bar test"
-      create id: 4, title: "bar4", description: "foo bar test"
-      create id: 5, title: "bar5", description: "foo bar test"
-      create id: 6, title: "bar6", description: "foo bar test"
-      create id: 7, title: "bar7", description: "foo bar test"
-      create id: 8, title: "bar8", description: "foo bar test"
-      create id: 9, title: "bar9", description: "foo bar test"
-      create id: 10, title: "bar10", description: "foo bar test"
-      create id: 11, title: "bar11", description: "foo bar test"
-      delete id: 11
-      index  id: 90, title: "barww"
-    end
+		end
 
-    Tirexs.Manage.refresh("bear_test", settings)
-    {_, _, body} = Tirexs.ElasticSearch.get("bear_test/_count", settings)
-    assert body["count"] == 11
-  end
+		Tirexs.Manage.refresh("bear_test", settings)
+		{_, _, body} = Tirexs.ElasticSearch.get("bear_test/_count", settings)
+		assert body["count"] == 2
+
+		Tirexs.Bulk.store [index: "bear_test", type: "document", id: 1, retry_on_conflict: 3], settings do
+			update doc: [title: "updated_title"]
+		end
+
+		Tirexs.Manage.refresh("bear_test", settings)
+
+		{_, _, body} = Tirexs.ElasticSearch.get("bear_test/document/1", settings)
+
+		# IO.puts inspect(body) #To do implement for 0.90.1
+
+	end
 end
