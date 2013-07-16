ad8c289a5000e6848bbb77e92b6bf2777d21c983
Opak Alex <opak.alexandr@gmail.com>
2013-07-16 21:33:41 +0300

    close #111

--- test/tirexs/percolator_test.exs
+++ test/tirexs/percolator_test.exs
@@ -14,26 +14,26 @@ defmodule Tirexs.PerlocatorTest do
   end
 
   test :with_param do
-    percolator = percolator [color: "blue"] do
-      query do
-        term "field1", "value1"
+      percolator = percolator [color: "blue"] do
+        query do
+          term "field1", "value1"
+        end
       end
+
+      assert percolator == [query: [term: [field1: "value1"]], color: "blue"]
     end
 
-    assert percolator == [query: [term: [field1: "value1"]], color: "blue"]
-  end
+    test :with_doc do
+      percolator = percolator do
+        doc do
+          [[field1: "value1"]]
+        end
+        query do
+          term "field1", "value1"
+        end
+      end
 
-  test :with_doc do
-    percolator = percolator do
-      doc do
-        [field1: "value1"]
-      end
-      query do
-        term "field1", "value1"
-      end
+      assert percolator == [doc: [field1: "value1"], query: [term: [field1: "value1"]]]
     end
-
-    assert percolator == [doc: [field1: "value1"], query: [term: [field1: "value1"]]]
-  end
 
 end
