4d621e40e80bbf007780bd9926845bffe4e3af6e
Zatvor <a.zatvornitskiy@gmail.com>
2014-03-12 14:17:34 +0200

    Bump 0.4.0 version

--- mix.exs
+++ mix.exs
@@ -2,16 +2,12 @@ defmodule Tirexs.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :tirexs,
-      version: "0.4.1-dev",
-      deps: deps ]
+    [ app: :tirexs, version: "0.4.0",deps: deps ]
   end
 
   def application, do: []
 
   defp deps do
-    [
-      {:jsex, github: "talentdeficit/jsex"}
-    ]
+    [ {:jsex, github: "talentdeficit/jsex"} ]
   end
 end
