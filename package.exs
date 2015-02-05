576a5229b704b2d57dee6b70e39671b0520e6b7c
Zatvobor <a.zatvornitskiy@gmail.com>
2015-02-05 19:30:23 +0200

    Update texts after changing the project home repo

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "tirexs", description: "An Elixir based DSL for operating the ElasticSearch cluster related stuff, such as indexes, scoped queries and so on.",
                  version: "0.5.0", keywords: ["Elixir", "Erlang", "ElasticSearch", "DSL", "library"],
                  maintainers: [[name: "support",
-                                email: "support@roundscope.com"]],
+                                email: "a.zatvornitskiy@gmail.com"]],
                  repositories: [[github: "roundscope/tirexs"]])
     
