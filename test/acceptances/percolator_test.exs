caf37bd105446045867ff79dc5cefbb57dcd2af5
Zatvor <a.zatvornitskiy@gmail.com>
2015-04-08 20:51:19 +0300

    Back to the green side a PercolatorTest, #138

--- test/acceptances/percolator_test.exs
+++ test/acceptances/percolator_test.exs
@@ -1,12 +1,19 @@
 Code.require_file "../../test_helper.exs", __ENV__.file
 defmodule Tirexs.PercolatorTest do
   use ExUnit.Case
+  import Tirexs.Bulk
   import Tirexs.Percolator
   require Tirexs.ElasticSearch
 
   test :percolator do
     settings = Tirexs.ElasticSearch.config()
     Tirexs.ElasticSearch.delete("my-index/.percolator/1", settings)
+
+    Tirexs.Bulk.store [index: "my-index", refresh: false], settings do
+      create id: 1, message: "foo bar test"
+      create id: 2, message: "bar bar test"
+      create id: 3, message: "Old bonsai tree is down"
+    end
 
     percolator = percolator [index: "my-index", name: 1] do
       query do
