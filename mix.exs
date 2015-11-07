9c63532cf2f50f77fb437f617d433741771d3619
Zatvobor <a.zatvornitskiy@gmail.com>
2015-11-07 20:33:02 +0200

    Bump version to '0.7.2'

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Tirexs.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :tirexs, version: "0.7.1", elixir: "~> 1.0.0", description: description, package: package, deps: deps ]
+    [ app: :tirexs, version: "0.7.2", elixir: "~> 1.0.0", description: description, package: package, deps: deps ]
   end
 
   def application do
