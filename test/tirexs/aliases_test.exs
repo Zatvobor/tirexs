6b65c8c4db7c2dc87878938456130cb1184584a0
Zatvobor <a.zatvornitskiy@gmail.com>
2016-01-31 14:56:52 +0200

    Tiny changes

--- test/tirexs/aliases_test.exs
+++ test/tirexs/aliases_test.exs
@@ -1,34 +1,49 @@
 Code.require_file "../../test_helper.exs", __ENV__.file
+
 defmodule Tirexs.AliasesTest do
   use ExUnit.Case
+
   import Tirexs.Manage.Aliases
   import Tirexs.Query.Filter
 
 
-  test :singl_alias do
+  test "remove alias" do
     aliases = aliases do
-      add index: "bear_test", alias: "my_alias1"
+      remove index: "bear_test", alias: "bear_test_alias"
     end
 
-    assert aliases == [actions: [[add: [index: "bear_test", alias: "my_alias1"]]]]
+    assert aliases == [actions: [
+      [remove: [
+        index: "bear_test", alias: "bear_test_alias"
+      ]]
+    ]]
   end
 
-  test :alias_with_filter do
+  test "add alias" do
+    aliases = aliases do
+      add index: "bear_test", alias: "bear_test_alias"
+    end
 
+    assert aliases == [actions: [
+      [add: [
+        index: "bear_test", alias: "bear_test_alias"
+      ]]
+    ]]
+  end
+
+  test "alias with filter" do
     filter = filter do
       term "user", "kimchy"
     end
 
     aliases = aliases do
-      add index: "bear_test", alias: "my_alias1"
-      add [index: "bear_test", alias: "my_alias2"] ++ filter
-      remove index: "bear_test", alias: "my_alias1"
+      add [index: "bear_test", alias: "bear_test_alias"] ++ filter
     end
 
     assert aliases == [actions: [
-      [add: [index: "bear_test", alias: "my_alias1"]],
-      [add: [index: "bear_test", alias: "my_alias2", filter: [term: [user: "kimchy"]]]],
-      [remove: [index: "bear_test", alias: "my_alias1"]]
+      [add: [
+        index: "bear_test", alias: "bear_test_alias", filter: [term: [user: "kimchy"]]
+      ]]
     ]]
   end
 end
