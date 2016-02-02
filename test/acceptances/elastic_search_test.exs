b6a9b192e1db90842fe63f8b8c5dbbc30afd7175
Marco Milanesi <kpanic@gmail.com>
2016-02-02 23:31:53 +0100

    Add an acceptance test for settings/mappings

--- test/acceptances/elastic_search_test.exs
+++ test/acceptances/elastic_search_test.exs
@@ -3,7 +3,7 @@ Code.require_file "../../test_helper.exs", __ENV__.file
 defmodule Acceptances.ElasticSearchTest do
 
   use ExUnit.Case
-  import Tirexs.Mapping, only: :macros
+  use Tirexs.Mapping, only: :macros
   import Tirexs.Bulk
   import Tirexs.ElasticSearch
   require Tirexs.ElasticSearch
@@ -135,5 +135,51 @@ defmodule Acceptances.ElasticSearchTest do
     assert Tirexs.Query.result(result, :count) == 1
     assert List.first(Tirexs.Query.result(result, :hits))[:_source][:id] == 2
 
+  end
+
+  test "create mapping and settings" do
+    es_settings = Tirexs.ElasticSearch.config()
+    delete("articles", es_settings)
+
+    index = [index: "articles", type: "article"]
+
+    settings do
+      analysis do
+        analyzer "autocomplete_analyzer",
+        [
+          filter: ["lowercase", "asciifolding", "edge_ngram"],
+          tokenizer: "whitespace"
+        ]
+        filter "edge_ngram", [type: "edgeNGram", min_gram: 1, max_gram: 15]
+      end
+    end
+
+    mappings do
+      indexes "country", type: "string"
+      indexes "city", type: "string"
+      indexes "suburb", type: "string"
+      indexes "road", type: "string"
+      indexes "postcode", type: "string", index: "not_analyzed"
+      indexes "housenumber", type: "string", index: "not_analyzed"
+      indexes "coordinates", type: "geo_point"
+      indexes "full_address", type: "string", analyzer: "autocomplete_analyzer"
+    end
+
+    Tirexs.Mapping.create_resource(index, es_settings)
+
+    Tirexs.Manage.refresh("articles", es_settings)
+
+    {:ok, 200, response} = Tirexs.ElasticSearch.get("articles", es_settings)
+
+    settings = response[:articles][:settings]
+    mappings = response[:articles][:mappings]
+
+    %{index: %{analysis: analyzer}} = settings
+
+    assert %{analyzer: %{autocomplete_analyzer: %{filter: ["lowercase", "asciifolding", "edge_ngram"], tokenizer: "whitespace"}}, filter: %{edge_ngram: %{max_gram: "15", min_gram: "1", type: "edgeNGram"}}} == analyzer
+
+    %{article: properties} = mappings
+
+    assert %{properties: %{city: %{type: "string"}, coordinates: %{type: "geo_point"}, country: %{type: "string"}, full_address: %{analyzer: "autocomplete_analyzer", type: "string"}, housenumber: %{index: "not_analyzed", type: "string"}, postcode: %{index: "not_analyzed", type: "string"}, road: %{type: "string"}, suburb: %{type: "string"}}} == properties
   end
 end
