28cb0f2c7948a705f549492b98565aa53898b41b
Zatvobor <a.zatvornitskiy@gmail.com>
2016-02-12 18:06:27 +0200

    Cleanup 'query_test.exs' and 'search_test.exs' (some tests are skipped)

--- test/tirexs/search_test.exs
+++ test/tirexs/search_test.exs
@@ -1,10 +1,13 @@
 Code.require_file "../../test_helper.exs", __ENV__.file
+
 defmodule Tirexs.SearchTest do
   use ExUnit.Case
+
   import Tirexs.Search
 
-  test :search_with_query_and_filter do
-    search = Tirexs.Search.search do
+
+  test "search w/ query, filter" do
+    search = search do
       query do
         term "tag", "wow"
       end
@@ -14,10 +17,12 @@ defmodule Tirexs.SearchTest do
       end
     end
 
-    assert search == [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]]]]
+    expected = [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]]]]
+    assert search == expected
   end
 
-  test :search_with_facets do
+  @tag skip: "facets were deprecated and removed in 2.0 core"
+  test "search w/ query, filter and facets" do
     search = search do
       query do
         term "tag", "wow"
@@ -37,10 +42,12 @@ defmodule Tirexs.SearchTest do
       end
     end
 
-    assert search == [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]], facets: [tagFacet: [terms: [field: "tag", size: 10, order: "term"]], keywordFacet: [terms: [field: "keywords", all_terms: true]]]]]
+    expected = [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]], facets: [tagFacet: [terms: [field: "tag", size: 10, order: "term"]], keywordFacet: [terms: [field: "keywords", all_terms: true]]]]]
+    assert search == expected
   end
 
-  test :complete_search do
+  @tag skip: "facets were deprecated and removed in 2.0 core"
+  test "search in general" do
     search = search [from: 0, size: 10, fields: [:tag, :article], explain: 5, version: true, min_score: 0.5] do
       query do
         term "tag", "wow"
@@ -95,9 +102,9 @@ defmodule Tirexs.SearchTest do
           test2: [script: "doc['my_field_name'].value * factor", params: [factor: 2]]
         ]
       end
-
     end
 
-      assert search == [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]], facets: [tagFacet: [terms: [field: "tag", size: 10, order: "term"]], keywordFacet: [terms: [field: "keywords", all_terms: true]]], highlight: [number_of_fragments: 3, fragment_size: 150, tag_schema: "styled"], suggest: [my_suggest_1: [fuzzy: [field: "body"]]], sort: [[post_date: [reverse: true]],[name: "desc"],[age: "desc"]], rescore: [query: [rescore_query: [match: [field1: [query: "the quick brown", type: "phrase", slop: 2]]], query_weight: 0.7, rescore_query_weight: 1.2], window_size: 50], script_fields: [test1: [script: "doc['my_field_name'].value * 2"], test2: [script: "doc['my_field_name'].value * factor", params: [factor: 2]]], from: 0, size: 10, fields: [:tag,:article], explain: 5, version: true, min_score: 0.5]]
+    expected = [search: [query: [term: [tag: "wow"]], filter: [term: [filter_tag: "wwoww"]], facets: [tagFacet: [terms: [field: "tag", size: 10, order: "term"]], keywordFacet: [terms: [field: "keywords", all_terms: true]]], highlight: [number_of_fragments: 3, fragment_size: 150, tag_schema: "styled"], suggest: [my_suggest_1: [fuzzy: [field: "body"]]], sort: [[post_date: [reverse: true]],[name: "desc"],[age: "desc"]], rescore: [query: [rescore_query: [match: [field1: [query: "the quick brown", type: "phrase", slop: 2]]], query_weight: 0.7, rescore_query_weight: 1.2], window_size: 50], script_fields: [test1: [script: "doc['my_field_name'].value * 2"], test2: [script: "doc['my_field_name'].value * factor", params: [factor: 2]]], from: 0, size: 10, fields: [:tag,:article], explain: 5, version: true, min_score: 0.5]]
+    assert search == expected
   end
 end
