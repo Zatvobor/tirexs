75fddcda398853259b726d553ea5caf4b3c1aec0
Zatvobor <a.zatvornitskiy@gmail.com>
2016-02-21 23:34:56 +0200

    Typespecs for HTTP

--- lib/tirexs/http.ex
+++ lib/tirexs/http.ex
@@ -40,51 +40,90 @@ defmodule Tirexs.HTTP do
       iex> put("/bear_test/my_type/1", %{version: 2}, %{user: "kimchy", id: 1})
   """
 
+  @typedoc "The Uniform Resource Identifier"
+  @type uri :: URI.t
+  @type url :: String.t
+
+  @typedoc "The response from server"
+  @type response :: { :ok, integer, map } | { :error, integer, map }
+
+  @typedoc "The boolean statement of server response"
+  @type response? :: boolean
+
+  @typedoc "The bung of server response"
+  @type response! :: response | no_return
+
+  @typedoc "The path as part of request url"
+  @type path :: String.t
+  @type url_or_path :: url | path
+  @type url_or_path_or_uri :: path | uri
+
+  @typedoc "The query params as a part of request url"
+  @type params :: String.t | [{atom, String.t}] | map
+
+  @typedoc "The response body"
+  @type body :: map
+
 
   @doc """
   Sends a HEAD request.
 
   """
+  @spec head(path, params, uri) :: response
   def head(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:head, url(path, params, uri))
   end
+  @spec head(path, uri) :: response
   def head(path, %URI{} = uri) when is_binary(path) do
     do_request(:head, url(path, uri))
   end
+  @spec head(url_or_path_or_uri, params) :: response
   def head(url_or_path_or_uri, params) do
     do_request(:head, url(url_or_path_or_uri, params))
   end
+  @spec head(url_or_path_or_uri) :: response
   def head(url_or_path_or_uri) do
     do_request(:head, url(url_or_path_or_uri))
   end
+  @spec head!(term, term, term) :: response!
   def head!(a, b, c), do: ok!(head(a, b, c))
+  @spec head!(term, term) :: response!
   def head!(a, b), do: ok!(head(a, b))
+  @spec head!(term) :: response!
   def head!(a), do: ok!(head(a))
 
   @doc """
   Sends a GET request.
 
   """
+  @spec get(path, params, uri) :: response
   def get(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:get, url(path, params, uri))
   end
+  @spec get(path, uri) :: response
   def get(path, %URI{} = uri) when is_binary(path) do
     do_request(:get, url(path, uri))
   end
+  @spec get(url_or_path_or_uri, params) :: response
   def get(url_or_path_or_uri, params) do
     do_request(:get, url(url_or_path_or_uri, params))
   end
+  @spec get(url_or_path_or_uri) :: response
   def get(url_or_path_or_uri) do
     do_request(:get, url(url_or_path_or_uri))
   end
+  @spec get!(term, term, term) :: response!
   def get!(a, b, c), do: ok!(get(a, b, c))
+  @spec get!(term, term) :: response!
   def get!(a, b), do: ok!(get(a, b))
+  @spec get!(term) :: response!
   def get!(a), do: ok!(get(a))
 
   @doc """
   Sends a PUT request.
 
   """
+  @spec put(path, params, uri, body) :: response
   def put(path, params, %URI{} = uri, []) when is_binary(path) do
     do_request(:put, url(path, params, uri))
   end
@@ -100,6 +139,7 @@ defmodule Tirexs.HTTP do
   def put(path, params, %URI{} = uri, body) when is_binary(path) and is_binary(body) do
     do_request(:put, url(path, params, uri), body)
   end
+  @spec put(path, uri, body) :: response
   def put(path, %URI{} = uri, []) when is_binary(path) do
     do_request(:put, url(path, uri))
   end
@@ -115,9 +155,11 @@ defmodule Tirexs.HTTP do
   def put(path, %URI{} = uri, body) when is_binary(path) and is_binary(body) do
     do_request(:put, url(path, uri), body)
   end
+  @spec put(path, params, uri) :: response
   def put(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:put, url(path, params, uri))
   end
+  @spec put(url_or_path_or_uri, params, body) :: response
   def put(url_or_path_or_uri, params, []) do
     do_request(:put, url(url_or_path_or_uri, params))
   end
@@ -133,9 +175,11 @@ defmodule Tirexs.HTTP do
   def put(url_or_path_or_uri, params, body) when is_binary(body) do
     do_request(:put, url(url_or_path_or_uri, params), body)
   end
+  @spec put(path, uri) :: response
   def put(path, %URI{} = uri) when is_binary(path) do
     do_request(:put, url(path, uri))
   end
+  @spec put(url_or_path_or_uri, body) :: response
   def put(url_or_path_or_uri, []) do
     do_request(:put, url(url_or_path_or_uri))
   end
@@ -151,21 +195,28 @@ defmodule Tirexs.HTTP do
   def put(url_or_path_or_uri, body) when is_binary(body) do
     do_request(:put, url(url_or_path_or_uri), body)
   end
+  @spec put(url_or_path_or_uri, params) :: response
   def put(url_or_path_or_uri, params) do
     do_request(:put, url(url_or_path_or_uri, params))
   end
+  @spec put(url_or_path_or_uri) :: response
   def put(url_or_path_or_uri) do
     do_request(:put, url(url_or_path_or_uri))
   end
+  @spec put!(term, term, term, term) :: response!
   def put!(a, b, c, d), do: ok!(put(a, b, c, d))
+  @spec put!(term, term, term) :: response!
   def put!(a, b, c), do: ok!(put(a, b, c))
+  @spec put!(term, term) :: response!
   def put!(a, b), do: ok!(put(a, b))
+  @spec put!(term) :: response!
   def put!(a), do: ok!(put(a))
 
   @doc """
   Sends a POST request.
 
   """
+  @spec post(path, params, uri, body) :: response
   def post(path, params, %URI{} = uri, []) when is_binary(path) do
     do_request(:post, url(path, params, uri))
   end
@@ -181,6 +232,7 @@ defmodule Tirexs.HTTP do
   def post(path, params, %URI{} = uri, body) when is_binary(path) and is_binary(body) do
     do_request(:post, url(path, params, uri), body)
   end
+  @spec post(path, uri, body) :: response
   def post(path, %URI{} = uri, []) when is_binary(path) do
     do_request(:post, url(path, uri))
   end
@@ -196,9 +248,11 @@ defmodule Tirexs.HTTP do
   def post(path, %URI{} = uri, body) when is_binary(path) and is_binary(body) do
     do_request(:post, url(path, uri), body)
   end
+  @spec post(path, params, uri) :: response
   def post(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:post, url(path, params, uri))
   end
+  @spec post(url_or_path_or_uri, params, body) :: response
   def post(url_or_path_or_uri, params, []) do
     do_request(:post, url(url_or_path_or_uri, params))
   end
@@ -214,12 +268,15 @@ defmodule Tirexs.HTTP do
   def post(url_or_path_or_uri, params, body) when is_binary(body) do
     do_request(:post, url(url_or_path_or_uri, params), body)
   end
+  @spec post(path, uri) :: response
   def post(path, %URI{} = uri) when is_binary(path) do
     do_request(:post, url(path, uri))
   end
+  @spec post(path, body) :: response
   def post(url_or_path_or_uri, []) do
     do_request(:post, url(url_or_path_or_uri))
   end
+  @spec post(url_or_path_or_uri, body) :: response
   def post(url_or_path_or_uri, body) when body == %{} do
     do_request(:post, url(url_or_path_or_uri))
   end
@@ -232,51 +289,67 @@ defmodule Tirexs.HTTP do
   def post(url_or_path_or_uri, body) when is_binary(body) do
     do_request(:post, url(url_or_path_or_uri), body)
   end
+  @spec post(url_or_path_or_uri, params) :: response
   def post(url_or_path_or_uri, params) do
     do_request(:post, url(url_or_path_or_uri, params))
   end
+  @spec post(url_or_path_or_uri) :: response
   def post(url_or_path_or_uri) do
     do_request(:post, url(url_or_path_or_uri))
   end
+  @spec post!(term, term, term, term) :: response!
   def post!(a, b, c, d), do: ok!(post(a, b, c, d))
+  @spec post!(term, term, term) :: response!
   def post!(a, b, c), do: ok!(post(a, b, c))
+  @spec post!(term, term) :: response!
   def post!(a, b), do: ok!(post(a, b))
+  @spec post!(term) :: response!
   def post!(a), do: ok!(post(a))
 
   @doc """
   Sends a DELETE request.
 
   """
+  @spec delete(path, params, uri) :: response
   def delete(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:delete, url(path, params, uri))
   end
+  @spec delete(path, uri) :: response
   def delete(path, %URI{} = uri) when is_binary(path) do
     do_request(:delete, url(path, uri))
   end
+  @spec delete(path, url_or_path_or_uri, params) :: response
   def delete(url_or_path_or_uri, params) do
     do_request(:delete, url(url_or_path_or_uri, params))
   end
+  @spec delete(path, url_or_path_or_uri) :: response
   def delete(url_or_path_or_uri) do
     do_request(:delete, url(url_or_path_or_uri))
   end
+  @spec delete!(term, term, term) :: response!
   def delete!(a, b, c), do: ok!(delete(a, b, c))
+  @spec delete!(term, term) :: response!
   def delete!(a, b), do: ok!(delete(a, b))
+  @spec delete!(term) :: response!
   def delete!(a), do: ok!(delete(a))
 
   @doc """
   Builds a complete URL by given parts.
 
   """
+  @spec url(path, params, uri) :: url
   def url(path, params, %URI{} = uri) when is_binary(path) and is_binary(params) do
     url(path, %URI{ uri | query: params })
   end
   def url(path, params, %URI{} = uri) when is_binary(path) do
     url(path, %URI{ uri | query: URI.encode_query(params) })
   end
+  @spec url(path, uri) :: url
   def url(path, %URI{} = uri) when is_binary(path) do
     { default, given } = { Tirexs.get_uri_env(), __normalize_path__(uri) }
     %URI{ __merge__(default, given) | path: __normalize_path__(path) } |> to_string
   end
+  @spec url(url_or_path, params) :: url
   def url(url_or_path, params) when is_binary(url_or_path) and is_binary(params) do
     location = %URI{ URI.parse(url_or_path) | query: params }
     { default, given } = { Tirexs.get_uri_env(), __normalize_path__(location) }
@@ -287,20 +360,24 @@ defmodule Tirexs.HTTP do
     { default, given } = { Tirexs.get_uri_env(), __normalize_path__(location) }
     __merge__(default, given) |> to_string
   end
+  @spec url(url_or_path) :: url
   def url(url_or_path) when is_binary(url_or_path) do
     { default, given } = { Tirexs.get_uri_env(), __normalize_path__(URI.parse(url_or_path)) }
     __merge__(default, given) |> to_string
   end
+  @spec url(uri) :: url
   def url(%URI{} = uri) do
     { default, given } = { Tirexs.get_uri_env(), __normalize_path__(uri) }
     __merge__(default, given) |> to_string
   end
+  @spec url() :: url
   def url(), do: url(Tirexs.get_uri_env())
 
   @doc """
   Returns `false` if `{ :error, _, _ } = response`, otherwise returns `true`.
 
   """
+  @spec ok?(response) :: response?
   def ok?(response) do
     case response do
       { :error, _, _ } -> false
@@ -312,6 +389,7 @@ defmodule Tirexs.HTTP do
   Raises `RuntimeError` if `{ :error, _, _ } = response`, otherwise returns `response` back.
 
   """
+  @spec ok!(response) :: response!
   def ok!(response) do
     case response do
       { :error, _, error } -> raise to_string(error)
