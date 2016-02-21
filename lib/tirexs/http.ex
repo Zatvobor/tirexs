b6a4edfb7ff2e9a8dbec37b6c3b79618735fa179
Zatvobor <a.zatvornitskiy@gmail.com>
2016-02-21 20:18:30 +0200

    Basic @moduledoc for HTTP module

--- lib/tirexs/http.ex
+++ lib/tirexs/http.ex
@@ -1,8 +1,50 @@
 defmodule Tirexs.HTTP do
-  @moduledoc false
+  @moduledoc """
+  A set of functions for working over HTTP.
+
+  This bare-bone module provides all you need for getting things done over elasticsearch REST API.
+  The functions designed to be handful for various possible use cases.
+
+  A set of request functions such as `head/1-3`, `get/1-3`, `put/1-4`, `post/1-4` and `delete/1-3` also
+  available with bangs functions. The bang! functions could raise a RuntimeError.
+
+      iex> get("unknown")
+      {:error, 404, _ }
+
+      iex> get!("unknown")
+      ** (RuntimeError) {"error":{"root_cause": [{"type":"index_not_found_exception" ... }]
+
+  The common set of allowed parameters are `path`, `params`, `uri` and `body`.
+
+      iex> { :ok, 200, _ } = get("/bear_test/my_type/1")
+      iex> { :ok, 200, _ } = put("/bear_test/my_type/1", [version: 2], [user: "kimchy", id: 1])
+      iex> { :ok, 200, _ } = put("/bear_test/my_type/1", [version: 2], %URI{ host: "example.com" }, [user: "kimchy", id: 1])
+
+  The `uri` parameter has a special purpose. You can use it for overriding any fields
+  from application's environment uri (`Tirexs.get_uri_env/0`).
+
+      iex> Tirexs.get_uri_env()
+      %URI{host: "127.0.0.1", port: 9200, scheme: "http"}
+
+      iex> get("/articles/document/1")
+      "http://127.0.0.1:9200/articles/document/1?_source=false"
+
+      iex> get("/articles/document/1", %URI{ port: 92 })
+      "http://127.0.0.1:92/articles/document/1?_source=false"
+
+  A query params could be as a part of `path` or used as a standalone `params`. A `body` param is allowed
+  to be a `%{}` or `[]`.
+
+      iex> put("/bear_test/my_type/1?version=2", [user: "kimchy", id: 1])
+      iex> put("/bear_test/my_type/1", [version: 2], [user: "kimchy", id: 1])
+      iex> put("/bear_test/my_type/1", %{version: 2}, %{user: "kimchy", id: 1})
+  """
 
 
-  @doc false
+  @doc """
+  Sends a HEAD request.
+
+  """
   def head(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:head, url(path, params, uri))
   end
@@ -19,7 +61,10 @@ defmodule Tirexs.HTTP do
   def head!(a, b), do: ok!(head(a, b))
   def head!(a), do: ok!(head(a))
 
-  @doc false
+  @doc """
+  Sends a GET request.
+
+  """
   def get(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:get, url(path, params, uri))
   end
@@ -36,7 +81,10 @@ defmodule Tirexs.HTTP do
   def get!(a, b), do: ok!(get(a, b))
   def get!(a), do: ok!(get(a))
 
-  @doc false
+  @doc """
+  Sends a PUT request.
+
+  """
   def put(path, params, %URI{} = uri, []) when is_binary(path) do
     do_request(:put, url(path, params, uri))
   end
@@ -114,7 +162,10 @@ defmodule Tirexs.HTTP do
   def put!(a, b), do: ok!(put(a, b))
   def put!(a), do: ok!(put(a))
 
-  @doc false
+  @doc """
+  Sends a POST request.
+
+  """
   def post(path, params, %URI{} = uri, []) when is_binary(path) do
     do_request(:post, url(path, params, uri))
   end
@@ -192,7 +243,10 @@ defmodule Tirexs.HTTP do
   def post!(a, b), do: ok!(post(a, b))
   def post!(a), do: ok!(post(a))
 
-  @doc false
+  @doc """
+  Sends a DELETE request.
+
+  """
   def delete(path, params, %URI{} = uri) when is_binary(path) do
     do_request(:delete, url(path, params, uri))
   end
@@ -209,7 +263,10 @@ defmodule Tirexs.HTTP do
   def delete!(a, b), do: ok!(delete(a, b))
   def delete!(a), do: ok!(delete(a))
 
-  @doc false
+  @doc """
+  Builds a complete URL by given parts.
+
+  """
   def url(path, params, %URI{} = uri) when is_binary(path) and is_binary(params) do
     url(path, %URI{ uri | query: params })
   end
@@ -239,6 +296,28 @@ defmodule Tirexs.HTTP do
     __merge__(default, given) |> to_string
   end
   def url(), do: url(Tirexs.get_uri_env())
+
+  @doc """
+  Returns `false` if `{ :error, _, _ } = response`, otherwise returns `true`.
+
+  """
+  def ok?(response) do
+    case response do
+      { :error, _, _ } -> false
+      _                -> true
+    end
+  end
+
+  @doc """
+  Raises `RuntimeError` if `{ :error, _, _ } = response`, otherwise returns `response` back.
+
+  """
+  def ok!(response) do
+    case response do
+      { :error, _, error } -> raise to_string(error)
+      _                    -> response
+    end
+  end
 
 
   @doc false
@@ -272,22 +351,6 @@ defmodule Tirexs.HTTP do
           end
         end
       _ -> :error
-    end
-  end
-
-  @doc false
-  def ok?(response) do
-    case response do
-      { :error, _, _ } -> false
-      _                -> true
-    end
-  end
-
-  @doc false
-  def ok!(response) do
-    case response do
-      { :error, _, error } -> raise to_string(error)
-      _                    -> response
     end
   end
 
