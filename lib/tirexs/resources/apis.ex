248cb82ee3ca531d35d861a0c4b79f051465713f
Marco Milanesi <kpanic@gmail.com>
2016-03-09 12:51:26 +0100

    Rename [Ii]ndicies to [Ii]indices

--- lib/tirexs/resources/apis.ex
+++ lib/tirexs/resources/apis.ex
@@ -37,49 +37,49 @@ defmodule Tirexs.Resources.APIs do
 
   """
 
-  alias Tirexs.Resources.Indicies
+  alias Tirexs.Resources.Indices
 
 
   ## Mapping Management
 
   @doc false
-  defdelegate [ _all_mapping() ], to: Indicies
-  defdelegate [ _mapping(), _mapping(a), _mapping(a, b), _mapping(a, b, c) ], to: Indicies
-  defdelegate [ _field_mapping(a), _field_mapping(a, b), _field_mapping(a, b, c),  _field_mapping(a, b, c)], to: Indicies
+  defdelegate [ _all_mapping() ], to: Indices
+  defdelegate [ _mapping(), _mapping(a), _mapping(a, b), _mapping(a, b, c) ], to: Indices
+  defdelegate [ _field_mapping(a), _field_mapping(a, b), _field_mapping(a, b, c),  _field_mapping(a, b, c)], to: Indices
 
   ## Index Settings
 
   @doc false
-  defdelegate [ _analyze(), _analyze(a), _analyze(a, b), _analyze(a, b, c) ], to: Indicies
-  defdelegate [ _warmer(), _warmer(a), _warmer(a, b), _warmer(a, b, c) ], to: Indicies
-  defdelegate [ _template(), _template(a), _template(a, b), _template(a, b, c) ], to: Indicies
-  defdelegate [ _settings(), _settings(a), _settings(a, b), _settings(a, b, c) ], to: Indicies
+  defdelegate [ _analyze(), _analyze(a), _analyze(a, b), _analyze(a, b, c) ], to: Indices
+  defdelegate [ _warmer(), _warmer(a), _warmer(a, b), _warmer(a, b, c) ], to: Indices
+  defdelegate [ _template(), _template(a), _template(a, b), _template(a, b, c) ], to: Indices
+  defdelegate [ _settings(), _settings(a), _settings(a, b), _settings(a, b, c) ], to: Indices
 
   ## Index Management
 
   @doc false
-  defdelegate [ _open(), _open(a), _open(a, b), _open(a, b, c) ], to: Indicies
-  defdelegate [ _close(), _close(a), _close(a, b), _close(a, b, c) ], to: Indicies
+  defdelegate [ _open(), _open(a), _open(a, b), _open(a, b, c) ], to: Indices
+  defdelegate [ _close(), _close(a), _close(a, b), _close(a, b, c) ], to: Indices
   ## Alias Management
 
   @doc false
-  defdelegate [ _aliases(), _aliases(a), _aliases(a, b), _aliases(a, b, c) ], to: Indicies
-  defdelegate [ _alias(), _alias(a), _alias(a, b), _alias(a, b, c) ], to: Indicies
+  defdelegate [ _aliases(), _aliases(a), _aliases(a, b), _aliases(a, b, c) ], to: Indices
+  defdelegate [ _alias(), _alias(a), _alias(a, b), _alias(a, b, c) ], to: Indices
 
   ## Status Management
 
   @doc false
-  defdelegate [ _refresh(), _refresh(a), _refresh(a, b), _refresh(a, b, c) ], to: Indicies
-  defdelegate [ _flush(), _flush(a), _flush(a, b), _flush(a, b, c) ], to: Indicies
-  defdelegate [ _forcemerge(), _forcemerge(a), _forcemerge(a, b), _forcemerge(a, b, c) ], to: Indicies
-  defdelegate [ _upgrade(), _upgrade(a), _upgrade(a, b), _upgrade(a, b, c) ], to: Indicies
-  defdelegate [ _cache_clear(), _cache_clear(a), _cache_clear(a, b), _cache_clear(a, b, c) ], to: Indicies
+  defdelegate [ _refresh(), _refresh(a), _refresh(a, b), _refresh(a, b, c) ], to: Indices
+  defdelegate [ _flush(), _flush(a), _flush(a, b), _flush(a, b, c) ], to: Indices
+  defdelegate [ _forcemerge(), _forcemerge(a), _forcemerge(a, b), _forcemerge(a, b, c) ], to: Indices
+  defdelegate [ _upgrade(), _upgrade(a), _upgrade(a, b), _upgrade(a, b, c) ], to: Indices
+  defdelegate [ _cache_clear(), _cache_clear(a), _cache_clear(a, b), _cache_clear(a, b, c) ], to: Indices
 
   ## Monitoring Management
 
   @doc false
-  defdelegate [ _stats(), _stats(a), _stats(a, b), _stats(a, b, c) ], to: Indicies
-  defdelegate [ _segments(), _segments(a), _segments(a, b), _segments(a, b, c) ], to: Indicies
-  defdelegate [ _recovery(), _recovery(a), _recovery(a, b), _recovery(a, b, c) ], to: Indicies
-  defdelegate [ _shard_stores(), _shard_stores(a), _shard_stores(a, b), _shard_stores(a, b, c) ], to: Indicies
+  defdelegate [ _stats(), _stats(a), _stats(a, b), _stats(a, b, c) ], to: Indices
+  defdelegate [ _segments(), _segments(a), _segments(a, b), _segments(a, b, c) ], to: Indices
+  defdelegate [ _recovery(), _recovery(a), _recovery(a, b), _recovery(a, b, c) ], to: Indices
+  defdelegate [ _shard_stores(), _shard_stores(a), _shard_stores(a, b), _shard_stores(a, b, c) ], to: Indices
 end
