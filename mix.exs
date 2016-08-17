65ef2fc49188c72311f0d67599cea090b2b69b4d
Zatvobor <a.zatvornitskiy@gmail.com>
2016-08-17 13:13:32 +0300

    Release v0.8.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tirexs.Mixfile do
     [
       app: :tirexs,
       name: "Tirexs",
-      version: "0.8.6",
+      version: "0.8.7",
       source_url: github,
       homepage_url: github,
       elixir: "~> 1.3.0",
