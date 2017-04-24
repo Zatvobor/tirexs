7c0fb1dccbfffb200655a535b198d0c66cd22ab4
Zatvobor <a.zatvornitskiy@gmail.com>
2017-04-24 17:49:23 +0300

    Bump version to v0.8.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tirexs.Mixfile do
     [
       app: :tirexs,
       name: "Tirexs",
-      version: "0.8.14",
+      version: "0.8.15",
       source_url: github(),
       homepage_url: github(),
       elixir: "~> 1.3.0 or ~> 1.4.1",
